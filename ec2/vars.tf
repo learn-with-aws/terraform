28f87bb3fd885eab71cdb7de8bf3b80f0b0d4263
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2020-11-23 18:31:39 +0530

    Update vars.tf

--- ec2/vars.tf
+++ ec2/vars.tf
@@ -2,9 +2,9 @@ variable "REGION" {
   default = "ap-south-1"
 }
 
-# variable "AMI" {
-#   default = "ami-026f33d38b6410e30"
-# }
+variable "AMI" {
+  default = ""
+}
 
 variable "INSTANCE_USER" {
   default = "centos"
