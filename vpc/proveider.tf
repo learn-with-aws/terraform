780dee1041e1dff573546332b487b61aeb7f6d52
learn-cloud <githubanand@gmail.com>
2021-01-11 22:14:20 +0530

    test

--- vpc/proveider.tf
+++ vpc/proveider.tf
@@ -1,3 +1,10 @@
 provider "aws" {
   region = var.AWS_REGION
 }
+terraform {
+  backend "s3" {
+    bucket = "terraform-remote-state-storage-s3-vpc"
+    key    = "vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
